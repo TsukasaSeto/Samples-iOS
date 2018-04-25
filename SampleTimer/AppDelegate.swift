238236dbdd2ae8122b99d0fa931426d39f4c3e91
Tsukasa Seto <Tsukasa.Seto@jp.ricoh.com>
2018-04-25 09:49:37 +0900

    Create sample timer application.

---
+++ SampleTimer/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SampleTimer
+//
+//  Created by Tsukasa Seto on 2018/04/25.
+//  Copyright © 2018年 Tsukasa Seto. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
